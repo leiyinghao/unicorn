64d53d5ca1468e8bc6a7aea0f2d3aa074b08d7f5
yhlei <leiyinghao.bj@gmail.com>
2013-06-05 11:30:35 +0800

    chapter4.1 安装引擎

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Unicorn::Application.config.secret_token = '49cf8c9913ca3a24d3c38d4e69e75469080d9a4c82b2eab7804cdf550da2e263d3da8e60a06b8b49926f8d8f2617c34df3898e70ba09615faa5281a9a6359d48'
